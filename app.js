89d4a857e6fe2873dc789ae1d772a8992b8526af
FabianT0000 <tfabiann@gmail.com>
2025-01-30 16:04:31 -0500

    primera carga de archivos

---
+++ app.js
@@ -0,0 +1,55 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigos = [];
+
+function agregarALista(amigoNombre) {
+    let listAmigos = document.querySelector('#listaAmigos');
+    let amigo = document.createElement('li');
+    amigo.textContent = amigoNombre;
+    listAmigos.appendChild(amigo);
+}
+
+function limpiar() {
+    let amigo = document.querySelector('#amigo');
+    amigo.value = '';
+}
+function limpiarListaMostrada() {
+    let listAmigos = document.querySelector('#listaAmigos');
+    listAmigos.replaceChildren();
+}
+
+function mostrarAmigo(amigosorteo) {
+    let resultado = document.querySelector('#resultado');
+    let amigosecreto = document.createElement('li');
+    amigosecreto.textContent = "Tú amigo secreto es : " + amigosorteo;
+    resultado.appendChild(amigosecreto);
+}
+function limpiarAmigoSecreto() {
+    let resultado = document.querySelector('#resultado');
+    resultado.replaceChildren();
+}
+
+//agregar amigo en lista para el sorte y en lista para mostrar en la interfaz
+function agregarAmigo() {
+    let amigo = document.querySelector('#amigo').value;
+    if (amigo == '') {
+        alert('Por favor,Agregar un nombre valido ');
+    } else {
+        agregarALista(amigo);
+        listaAmigos.push(amigo);
+        limpiar();
+    }
+}
+
+//Sorteo del amigo secreto 
+function sortearAmigo() {
+    if (listaAmigos.length == 0) {
+        alert('Primero debes agregar tus amigos secretos');
+    } else {
+        limpiarAmigoSecreto();
+        let amigoSecreto = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+        limpiarListaMostrada();
+        mostrarAmigo(amigoSecreto);
+    }
+
+}
